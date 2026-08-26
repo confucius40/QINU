0f8d416d870ace8bffafdbf1abdf019905ecfbed
confucius40 <git@mail.katon15.xyz>
2026-08-26 22:36:06 +0200

    feat: print 'QINU'

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,17 @@
+#define VGA_MEMORY ((volatile unsigned short *)0xB8000)
+
+static void console_write(const char *text)
+{
+    unsigned int i;
+
+    for (i = 0; text[i] != '\0'; i++)
+        VGA_MEMORY[i] = (unsigned short)text[i] | ((unsigned short)0x0F << 8);
+}
+
 void long_mode_entry(void)
 {
+    console_write("QINU");
+
     for (;;)
         ;
 }
